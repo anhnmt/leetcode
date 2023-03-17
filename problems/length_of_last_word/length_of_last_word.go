3262e62caf7e70c1222afac79c18ddb3933bc2b0
Anh Nguyen <anhng.dev@gmail.com>
2023-03-17 11:53:29 +0700

    Optimize length_of_last_word

--- problems/length_of_last_word/length_of_last_word.go
+++ problems/length_of_last_word/length_of_last_word.go
@@ -3,17 +3,15 @@ package length_of_last_word
 func lengthOfLastWord(s string) int {
 	c := 0
 	for i := len(s) - 1; i >= 0; i-- {
-		if c > 0 && s[i] == ' ' {
-			return c
-		}
-
-		if s[i] == ' ' && c == 0 {
-			s = s[:i]
+		if s[i] != ' ' {
+			c++
 			continue
 		}
 
-		c++
+		if c > 0 {
+			break
+		}
 	}
 
-	return len(s)
+	return c
 }
