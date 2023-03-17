46dcd25ec2bd62315ab746849c6934855996bca3
Anh Nguyen <anhng.dev@gmail.com>
2023-03-17 22:28:30 +0700

    Update search_insert_position

---
+++ problems/35.search_insert_position/search_insert_position_test.go
@@ -0,0 +1,49 @@
+package search_insert_position
+
+import (
+	"testing"
+)
+
+func Test_searchInsert(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "exam 1",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 5,
+			},
+			want: 2,
+		},
+		{
+			name: "exam 2",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 2,
+			},
+			want: 1,
+		},
+		{
+			name: "exam 3",
+			args: args{
+				nums:   []int{1, 3, 5, 6},
+				target: 7,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchInsert(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("searchInsert() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
